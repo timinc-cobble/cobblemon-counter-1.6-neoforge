3a80f4ed46fc299b5a879df975fcc833d1f18db4
tmetcalfe89 <tmetcalfe89@gmail.com>
2025-05-17 17:58:00 -0700

    fast forward to 1.4.0 from fabric
    https://github.com/timinc-cobble/cobblemon-counter-1.6-fabric/releases

---
+++ src/main/kotlin/us/timinc/mc/cobblemon/counter/spawning_conditions/CountRequirement.kt
@@ -0,0 +1,15 @@
+package us.timinc.mc.cobblemon.counter.spawning_conditions
+
+import com.cobblemon.mod.common.util.asIdentifierDefaultingNamespace
+import net.minecraft.resources.ResourceLocation
+import us.timinc.mc.cobblemon.counter.api.CounterType
+
+data class CountRequirement(
+    val type: CounterType,
+    val amount: Int,
+    val species: String,
+    val form: String?,
+) {
+    val speciesRl: ResourceLocation
+        get() = species.asIdentifierDefaultingNamespace()
+}
